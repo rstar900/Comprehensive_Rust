8db43bbb0c3f66665d585a8f7df63b65526a7e21
Rachit Garg <rachitgarg900@gmail.com>
2024-04-28 17:02:02 +0200

    Create 01_Fibonacci.rs

---
+++ 01_Fibonacci.rs
@@ -0,0 +1,14 @@
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        // The base case.
+        1
+    } else {
+        // The recursive case.
+        fib(n-1) + fib(n-2)
+    }
+}
+
+fn main() {
+    let n = 20;
+    println!("fib({n}) = {}", fib(n));
+}
