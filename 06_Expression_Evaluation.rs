dbfac158b7d8ddc2f9dd3d0e0e4b7b8a9e6ac9b5
Rachit Garg <rachitgarg900@gmail.com>
2024-05-01 20:12:47 +0200

    Created 06_Expression_Evaluation.rs

--- 06_Expression_Evaluation.rs
+++ 06_Expression_Evaluation.rs
@@ -19,15 +19,80 @@ enum Expression {
 
 fn eval(e: Expression) -> Result<i64, String> {
     
-    match &e {
+    match e {
       // Base case
       Expression::Value(x) => Ok(x),
       // Non base cases
-      Expression::Op{op: Operation::Add, left, right} => todo(),
-      Expression::Op{op: Operation::Sub, left, right} => todo!(),
-      Expression::Op{op: Operation::Mul, left, right} => todo!(),
-      Expression::Op{op: Operation::Div, left, right} => todo!(),
+      Expression::Op{op: Operation::Add, left, right} => {
+          // Evaluate left and right expressions if not proceed and do addition
+          let left_result = eval(*left);
+          let right_result = eval(*right);
+          
+          //Check for any errors and return Err if that is the case
+          if let Err(msg) = left_result {
+              return Err(msg)
+          }
+          if let Err(msg) = right_result {
+              return Err(msg)
+          }
+          
+          // if not proceed and do addition
+          Ok(left_result.unwrap() + right_result.unwrap())
+      },
+      Expression::Op{op: Operation::Sub, left, right} => {
+          // Evaluate left and right expressions if not proceed and do addition
+          let left_result = eval(*left);
+          let right_result = eval(*right);
+          
+          //Check for any errors and return Err if that is the case
+          if let Err(msg) = left_result {
+              return Err(msg)
+          }
+          if let Err(msg) = right_result {
+              return Err(msg)
+          }
+          
+          // if not proceed and do subtraction
+          Ok(left_result.unwrap() - right_result.unwrap())
+      },
+      Expression::Op{op: Operation::Mul, left, right} => {
+          // Evaluate left and right expressions if not proceed and do addition
+          let left_result = eval(*left);
+          let right_result = eval(*right);
+          
+          //Check for any errors and return Err if that is the case
+          if let Err(msg) = left_result {
+              return Err(msg)
+          }
+          if let Err(msg) = right_result {
+              return Err(msg)
+          }
+          
+          // if not proceed and do multiplication
+          Ok(left_result.unwrap() * right_result.unwrap())
+      },
+      Expression::Op{op: Operation::Div, left, right} => {
+          // Evaluate left and right expressions if not proceed and do addition
+          let left_result = eval(*left);
+          let right_result = eval(*right);
+          
+          //Check for any errors and return Err if that is the case
+          if let Err(msg) = left_result {
+              return Err(msg)
+          }
+          if let Err(msg) = right_result {
+              return Err(msg)
+          }
+          
+          // if not proceed and do division (check for divide by zero error as well)
+          // * and as_ref() used to avoid move during unwrap() and deref the returned reference to result
+          if *right_result.as_ref().unwrap() == 0 {
+              return Err(String::from("division by zero"));
+          }
+          Ok(left_result.unwrap() / right_result.unwrap())
+      }
     }
+    
 }
 
 #[test]
