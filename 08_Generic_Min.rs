b22912c7977306bb668c586340952e731e883513
Rachit Garg <rachitgarg900@gmail.com>
2024-05-05 02:08:28 +0200

    Create 08_Generic_Min.rs

---
+++ 08_Generic_Min.rs
@@ -0,0 +1,16 @@
+use std::cmp::Ordering;
+
+fn min<T: Ord>(left: T, right: T) -> T{
+    left.min(right)
+}
+
+fn main() {
+    assert_eq!(min(0, 10), 0);
+    assert_eq!(min(500, 123), 123);
+
+    assert_eq!(min('a', 'z'), 'a');
+    assert_eq!(min('7', '1'), '1');
+
+    assert_eq!(min("hello", "goodbye"), "goodbye");
+    assert_eq!(min("bat", "armadillo"), "armadillo");
+}
